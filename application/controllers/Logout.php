7aa9b071a82f7b535385681c5439eabdc62a5d0a
riganrahmadin <riganrahmadin8@gmail.com>
2023-07-12 10:48:52 +0700

    SPK ADD

---
+++ application/controllers/Logout.php
@@ -0,0 +1,19 @@
+<?php
+
+class Logout extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$this->flashmsg('<i class="glyphicon glyphicon-remove"></i> Anda berhasil logout!', 'success');
+		$this->session->unset_userdata('username');
+		$this->session->unset_userdata('id_role');
+		redirect('login');
+		exit;
+	}
+}
